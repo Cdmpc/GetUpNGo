2144fdc5604c4e58867a067804924014bba058bf
Cdmpc <carlosm7.cdmp@gmail.com>
2023-03-31 16:51:33 -0400

    created home page

--- getupngo/src/App.js
+++ getupngo/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/" element={[<HomePage />]} />
       </Routes>
     </div>
   );
---
+++ getupngo/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PrimeReact from "primereact/api";
+
+/** RENDERS THE HOMEPAGE AND CREATES TWO BUTTONS TO LOGIN OR CONTINUE AS A GUEST. */
+
+export default function HomePage() {
+  return (
+    <>
+      <header>GETUPN'GO</header>
+    </>
+  );
+}
