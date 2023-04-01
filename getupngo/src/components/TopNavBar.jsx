6dd747dfc8b7c3331ef81be1d1f263e813a7050d
Cdmpc <carlosm7.cdmp@gmail.com>
2023-04-01 01:15:09 -0400

    Made changes to HomePage

--- getupngo/src/components/TopNavBar.jsx
+++ getupngo/src/components/TopNavBar.jsx
@@ -1,8 +1,8 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import GGLogo from "../images/GGLogo.png";
 import { Menubar } from "primereact/menubar";
 import { Button } from "primereact/button";
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 /** ITEMS AND LOGOS OF THE NAVBAR COMPONENT. */
 const NavBarItems = [
@@ -10,24 +10,32 @@ const NavBarItems = [
     label: "How It Works",
     icon: "pi pi-question-circle",
   },
-  { label: "Pricing", icon: "pi pi-dollar" },
+  {
+    label: "Pricing",
+    icon: "pi pi-dollar",
+    style: { color: "red" },
+  },
   {
     label: "Find a bike",
     icon: "pi pi-search",
+    style: { color: "white" },
   },
 ];
 
 const TopNavBar = () => {
-  let nav = useNavigate();
   return (
     <React.Fragment>
       <Menubar
-        style={{ padding: "15px" }}
+        style={{
+          padding: "15px",
+          boxShadow: "1px 5px 12px #131326",
+        }}
         model={NavBarItems}
         start={
           <Link to="/">
             <img
               src={GGLogo}
+              alt="Bike and GetUp'N'Go Text underneath"
               height={100}
               width={160}
               style={{ padding: "5px" }}
--- getupngo/src/pages/HomePage.jsx
+++ getupngo/src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TopNavBar from "../components/TopNavBar";
+import Toronto from "../images/Toronto.jpg";
 
 export default function HomePage() {
   return (
@@ -7,6 +8,37 @@ export default function HomePage() {
       <div className="header-bar">
         <TopNavBar />
       </div>
+      <div
+        className="image-with-text"
+        style={{
+          position: "relative",
+          textAlign: "center",
+        }}
+      >
+        <img
+          src={Toronto}
+          alt="Toronto Skyline at night"
+          style={{
+            width: "100%",
+            height: "750px",
+            filter: "brightness(60%)",
+          }}
+        />
+        <h1
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-90%, -90%)",
+            color: "white",
+            fontSize: "5rem",
+            fontFamily: "Gotham Light",
+          }}
+        >
+          Grab the city by the handlebars
+        </h1>
+      </div>
+      <div></div>
     </React.Fragment>
   );
 }
