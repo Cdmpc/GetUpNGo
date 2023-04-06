2eae9976fb9a98c2b1f8a24b6239309d9ca51906
Cdmpc <carlosm7.cdmp@gmail.com>
2023-04-05 23:23:16 -0400

    Created a basic connection to FE and BE

--- getupngo/src/pages/HomePage.jsx
+++ getupngo/src/pages/HomePage.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Toronto from "../images/Toronto.jpg";
 import TopNavBar from "../components/TopNavBar";
 import BackgText from "../components/BgText.jsx";
 
 export default function HomePage() {
+  const [apiRep, setAPIRep] = useState("");
+
+  const callAPI = () => {
+    fetch("http://localhost:9000/testAPI")
+      .then((res) => res.text())
+      .then((res) => setAPIRep(res));
+  };
+
+  useEffect(() => {
+    callAPI();
+  }, []);
+
   return (
     <React.Fragment>
       <div className="nav-bar">
@@ -17,6 +29,9 @@ export default function HomePage() {
           TitleText="Grab Toronto by the handlebars"
           TitleSub="Immerse yourself in the noise of the Scotiabank Arena, the glow of the CN Tower to the neon of Nathan Phillips Square."
         />
+      </div>
+      <div className="backend-test">
+        <p>{apiRep}</p>
       </div>
     </React.Fragment>
   );
