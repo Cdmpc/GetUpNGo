51b60bfc66ad04cbed93a679ba072e9d7cc955c4
Cdmpc <carlosm7.cdmp@gmail.com>
2023-04-07 15:12:25 -0400

    Added Dynamic Map Page

--- getupngo/src/App.js
+++ getupngo/src/App.js
@@ -6,6 +6,7 @@ import { Routes, Route } from "react-router-dom";
 import "primereact/resources/themes/lara-light-teal/theme.css"; //theme
 import "primereact/resources/primereact.min.css"; //core css
 import "primeicons/primeicons.css"; //icons
+import "mapbox-gl/dist/mapbox-gl.css"; //Mapbox Styling
 import HowItWorks from "./pages/HowItWorks";
 import Pricing from "./pages/Pricing";
 import LoginPage from "./pages/LoginPage";
